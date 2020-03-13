dfcc1dfbc53a4909420c809df07ee506d6b2a0ee
Mohit Dubey <md.visiesoft@gmail.com>
2020-03-13 13:28:51 +0530

    megatechnica scrapy script

---
+++ megatechnica/spiders/megatech.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import os
+import csv
+import glob
+from openpyxl import Workbook
+
+
+class MegatechSpider(scrapy.Spider):
+    name = 'megatech'
+    allowed_domains = ['megatechnica.ge']
+    start_urls = ['https://megatechnica.ge/ge/product', 'https://megatechnica.ge/en/sadenebi',
+                  'https://megatechnica.ge/en/USB%20Flash%20drive']
+
+    def start_requests(self):
+        yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
+
+    def parse(self, response):
+        for product in response.xpath('//li[@itemprop="itemListElement"]'):
+            product_link = product.css("a.img_link::attr(href)").get()
+            ab_link = response.urljoin(product_link)
+            yield scrapy.Request(url=ab_link, callback=self.parse_page)
+
+    def parse_page(self, response):
+        url = response.url
+        # name = response.css('div.detail>h2::text').extract()
+        name = response.xpath('//div[@class="detail"]/h2[@itemprop="name"]/text()').extract()
+        if name:
+            name = self.rm_whilespace(name)
+        manufacturer = response.css('div.detail span.manufacturer a span::text').extract()
+        if manufacturer:
+            manufacturer = self.rm_whilespace(manufacturer)
+            manufacturer = manufacturer.strip()
+        cat_list = response.css('div.subheader a>span.filter::text').extract()
+        cat = ''
+        sub_cat = ''
+        if cat_list:
+            if len(cat_list) == 2:
+                cat = cat_list[1]
+                sub_cat = 'NA'
+            else:
+                cat = cat_list[1]
+                sub_cat = cat_list[2]
+
+        # description = response.css('div.detail div.description::text').extract()
+        # description = response.xpath('normalize-space(.//*[@class="description"])').extract()
+        description = response.xpath(
+            '//div[@class="detail"]/div[@class="description"]/descendant-or-self::*/text()').extract()
+        if description:
+            description = self.rm_whilespace(description)
+
+        price = response.xpath('//div[@class="prices"]/span[@class="price"]/text()').extract()
+        price_symbol = response.xpath('//div[@class="prices"]/span[@class="gel"]/text()').extract()
+        if price:
+            price = self.rm_whilespace(price)
+
+        if price_symbol:
+            price_symbol = self.rm_whilespace(price_symbol)
+
+        images = response.xpath(
+            '//div[@class="images"]/ul[contains(@class, "slider-for")]/li[contains(@data-fancybox, "images")]/@href').extract()
+        if images:
+            images = [nn_.replace('\n', '') for nn_ in images]
+            images = [nn_.strip() for nn_ in images]
+            images = [response.urljoin(nn_) for nn_ in images]
+            images = filter(None, images)
+            images = ', '.join(images)
+            images = images
+
+        manufacturer_img = response.xpath(
+            '//div[@class="specifications"]/span[@class="manufacturer"]/@style').extract_first()
+
+        if manufacturer_img:
+            manufacturer_img = manufacturer_img.replace('background-image:url(', '')
+            manufacturer_img = manufacturer_img.replace(');', '')
+            manufacturer_img = response.urljoin(manufacturer_img)
+
+        specifications = response.xpath('//div[@class="description"]/table/tbody/tr')
+        spec = []
+        for td in specifications:
+            spec_head = td.xpath('td[1]//text()').extract()
+            spec_value = td.xpath('td[2]//text()').extract()
+            if spec_head:
+                spec_head = self.rm_whilespace(spec_head)
+            else:
+                spec_head = 'NA'
+            if spec_value:
+                spec_value = self.rm_whilespace(spec_value)
+            else:
+                spec_value = 'NA'
+            if spec_head:
+                temp = str(spec_head) + ':' + str(spec_value)
+                spec.append(temp)
+
+        if spec:
+            final_spec = '|'.join(spec)
+        else:
+            final_spec = response.xpath(
+                '//div[@class="specifications"]/div[@class="description"]/descendant-or-self::*/text()').extract()
+            final_spec = self.rm_whilespace(final_spec)
+
+        yield {
+            'product_name': name,
+            'manufacturer': manufacturer,
+            'price': price,
+            'price_symbol': price_symbol,
+            'cat_list': cat_list,
+            'category': cat.strip(),
+            'sub_category': sub_cat.strip(),
+            'description': description,
+            'manufacturer_img': manufacturer_img,
+            'specifications': final_spec,
+            'images': images,
+            'url': url
+        }
+
+    @staticmethod
+    def rm_whilespace(query_term):
+        if query_term:
+            None_ = [nn_.replace('\n', '') for nn_ in query_term]
+            None_ = [nn_.strip() for nn_ in None_]
+            None_ = filter(None, None_)
+            None_ = ' '.join(None_)
+            ret_value = None_
+            return ret_value
+        return query_term
+
+    def close(self, reason):
+        csv_file = max(glob.iglob('*.csv'), key=os.path.getctime)
+
+        wb = Workbook()
+        ws = wb.active
+
+        with open(csv_file, 'r', encoding='utf-8') as f:
+            for row in csv.reader(f):
+                ws.append(row)
+
+        wb.save(csv_file.replace('.csv', '') + '.xlsx')
